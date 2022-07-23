822a02e9068dbea4e213056bd895f5cd6ceb6c13
Lonewolf239 <semga05@mail.ru>
2022-07-23 17:14:25 +0300

    Add game files

---
+++ base_escape_2.0_rus.cpp
@@ -0,0 +1,20 @@
+﻿#include <clocale>
+#include "start.h"
+#include "Locations.h"
+#include "window.h"
+#include "cycle.h"
+#include "close.h"
+#include "door.h"
+#include "escape.h"
+#include "carpter.h"
+using namespace  std;
+void start(int start);
+int main() {
+	setlocale(LC_CTYPE, "rus");
+	int st = 0;
+	cout << "Добро пожаловать в мою первую тестовую игру\nBase_escape_1.0_rus\nВведите число (1) для старта:" << endl;
+	cin >> st;
+	if (st == 1)
+		start(1);
+	return 0;
+}
---
+++ escape.h
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+void escape(int esc) {
+	if (esc == 1) 
+		cout << "Спасибо за игру!\nНадеюсь, вам понравилась моя первая попытка создания игр. Удачи, друг <3\n" << endl;
+	exit;
+}
